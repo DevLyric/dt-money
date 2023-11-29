c4ada5af81a33cb0b5051d4b8524b9e4d6013705
devLyric <lyricsan@hotmail.com>
2023-11-29 08:56:19 -0300

    exibindo dashboard e adicionando slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import Slider from "react-slick";
 import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import { SliderSettings } from "./type/sliderSettings";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 
 function App() {
+    const settings: SliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
     return (
-        <div>
+        <div className="overflow-hidden">
             <Header />
+            <Slider
+                {...settings}
+                className="container mx-auto flex items-center justify-between gap-8 px-5"
+            >
+                <Dashboard title="Entradas" value="R$ 17.400,00" />
+                <Dashboard title="Saidas" value="R$ 1.259,00" />
+                <Dashboard title="Total" value="R$ 16.141,00" />
+            </Slider>
         </div>
     );
 }
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,18 @@
+interface DashboardProps {
+    title: string;
+    value: string;
+}
+
+function Dashboard({ title, value }: DashboardProps) {
+    return (
+        <div className="container mx-auto h-28 p-5 min-w-[296px] flex flex-col justify-between shadow-md">
+            <div className="flex items-center justify-between">
+                <p>{title}</p>
+                <span>icon</span>
+            </div>
+            <span className="text-xl font-medium">{value}</span>
+        </div>
+    );
+}
+
+export default Dashboard;
