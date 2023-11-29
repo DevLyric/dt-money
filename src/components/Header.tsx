02d40976a1cb83b460df60aef23eb5f63c9f9954
devLyric <lyricsan@hotmail.com>
2023-11-29 07:50:28 -0300

    trabalhando no header

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import Logo from "./icons/Logo";
+
+function Header() {
+    return (
+        <div className="container mx-auto h-20 flex items-center justify-between px-5">
+            <div className="flex items-center gap-3">
+                <Logo />
+                <h1 className="font-semibold text-lg">DT Money</h1>
+            </div>
+            <button className="bg-[#00875f] text-white font-medium py-2 w-32 rounded">
+                Nova transição
+            </button>
+        </div>
+    );
+}
+
+export default Header;
